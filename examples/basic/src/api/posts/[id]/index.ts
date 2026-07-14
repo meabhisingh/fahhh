ebd6bc5c225004db699a49a8b3caf15a8fe68e64
Abhishek <meabhisingh25@gmail.com>
2026-07-14 06:54:01 +0530

    fix: Logo correctly updated in example and template

--- examples/basic/src/api/posts/[id]/index.ts
+++ examples/basic/src/api/posts/[id]/index.ts
@@ -2,19 +2,19 @@ import type { ApiRequest } from "fahhh";
 import { notFound } from "fahhh";
 
 interface Params {
-  id: string;
+	id: string;
 }
 
 export async function GET(req: ApiRequest<undefined, Params>) {
-  const posts = {
-    "1": { id: "1", title: "Hello from fahhh" },
-  };
+	const posts = {
+		"1": { id: "1", title: "Hello from fahhh" },
+	};
 
-  const post = posts[req.params.id as keyof typeof posts];
+	const post = posts[req.params.id as keyof typeof posts];
 
-  if (!post) {
-    return notFound();
-  }
+	if (!post) {
+		return notFound();
+	}
 
-  return post;
+	return post;
 }
--- examples/basic/src/api/users/index.ts
+++ examples/basic/src/api/users/index.ts
@@ -1,26 +1,26 @@
 import type { ApiRequest } from "fahhh";
 
 interface CreateUserBody {
-  name: string;
+	name: string;
 }
 
 const users = [
-  { id: "1", name: "Angena Ghatram" },
-  { id: "2", name: "Naine Vaktram" },
+	{ id: "1", name: "Angena Ghatram" },
+	{ id: "2", name: "Naine Vaktram" },
 ];
 
 export async function GET() {
-  return users;
+	return users;
 }
 
 export async function POST(req: ApiRequest<CreateUserBody>) {
-  const body = await req.json();
+	const body = await req.json();
 
-  const user = {
-    id: String(users.length + 1),
-    name: body.name,
-  };
+	const user = {
+		id: String(users.length + 1),
+		name: body.name,
+	};
 
-  users.push(user);
-  return user;
+	users.push(user);
+	return user;
 }
